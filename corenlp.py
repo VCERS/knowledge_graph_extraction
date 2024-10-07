2a43e83958f2460bee355c301ba36bdd6bb6bff4
breadbread1984 <breadbread1984@163.com>
2024-10-07 17:56:16 +0800

    update

--- corenlp.py
+++ corenlp.py
@@ -12,7 +12,7 @@ class CoreNLP(object):
     if not exists('stanford-corenlp-4.5.7-models-english.jar'): download('https://search.maven.org/remotecontent?filepath=edu/stanford/nlp/stanford-corenlp/4.5.7/stanford-corenlp-4.5.7-models-english.jar', out = '.')
     jpype.startJVM(classpath = ['/usr/share/java/org.jpype-1.3.0.jar','stanford-corenlp-4.5.7-models-english.jar'])
     Properties = jpype.JClass('java.util.Properties')
-    StanfordCoreNLP = jpype.JClass('edu.standford.nlp.pipeline.StanfordCoreNLP')
+    StanfordCoreNLP = jpype.JClass('edu.stanford.nlp.pipeline.StanfordCoreNLP')
     self.CoreDocument = jpype.JClass('edu.stanford.nlp.pipeline.CoreDocument')
     self.String = jpype.JClass('java.lang.String')
     props = Properties()
