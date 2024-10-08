c7d17ee0440de39ee7c7a71f42b5ddc442199eb8
breadbread1984 <breadbread1984@163.com>
2024-10-08 06:18:49 +0000

    update

--- corenlp.py
+++ corenlp.py
@@ -9,8 +9,10 @@ from wget import download
 
 class CoreNLP(object):
   def __init__(self,):
-    if not exists('stanford-corenlp-4.5.7-models-english.jar'): download('https://search.maven.org/remotecontent?filepath=edu/stanford/nlp/stanford-corenlp/4.5.7/stanford-corenlp-4.5.7-models-english.jar', out = '.')
-    jpype.startJVM(classpath = ['/usr/share/java/org.jpype-1.3.0.jar','stanford-corenlp-4.5.7-models-english.jar'])
+    if not exists('stanford-corenlp-4.5.7.jar'): download('https://repo1.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/4.5.7/stanford-corenlp-4.5.7.jar', out = '.')
+    if not exists('stanford-corenlp-4.5.7-models.jar'): download('https://repo1.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/4.5.7/stanford-corenlp-4.5.7-models.jar', out = '.')
+    if not exists('stanford-corenlp-4.5.7-models-english.jar'): download('https://repo1.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/4.5.7/stanford-corenlp-4.5.7-models-english.jar', out = '.')
+    jpype.startJVM(classpath = ['/usr/share/java/org.jpype-1.3.0.jar','stanford-corenlp-4.5.7.jar','stanford-corenlp-4.5.7-models.jar','stanford-corenlp-4.5.7-models-english.jar'])
     Properties = jpype.JClass('java.util.Properties')
     StanfordCoreNLP = jpype.JClass('edu.stanford.nlp.pipeline.StanfordCoreNLP')
     self.CoreDocument = jpype.JClass('edu.stanford.nlp.pipeline.CoreDocument')
